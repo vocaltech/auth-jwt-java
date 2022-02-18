745e9297f0ad85c777a8e38f356d603bf164a553
vocaltech <it@vocaltech.fr>
2022-02-18 21:30:52 +0100

    refactor issueJwt() and decodeJwt()

--- src/main/java/fr/vocaltech/auth/jwt/JwtUtils.java
+++ src/main/java/fr/vocaltech/auth/jwt/JwtUtils.java
@@ -4,6 +4,7 @@ import com.google.gson.JsonObject;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 
+import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.DatatypeConverter;
 import java.security.*;
@@ -16,9 +17,8 @@ public class JwtUtils {
      * Generate a secret key HS256
      * @return the secret key
      */
-    public static String generateSecretKeyHS256() {
-        Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
-        return Base64.getEncoder().encodeToString(key.getEncoded());
+    public static SecretKey generateSecretKeyHS256() {
+        return Keys.secretKeyFor(SignatureAlgorithm.HS256);
     }
 
     public static String[] generateRSAPEM(KeyPair rsaKeyPair) {
@@ -66,13 +66,11 @@ public class JwtUtils {
      *
      * @param payload the payload in JSON format
      * @param expiresAfter expiresAfter value
-     * @param privateKey the private key to use to sign the payload
+     * @param privateOrSecretKey the private/secret key to use to sign the payload
      * @param signatureAlgorithm the algorithm to be used for the signature
      * @return the Jwt token
      */
-    public static String issueJwt(JsonObject payload, Long expiresAfter, String privateKey, SignatureAlgorithm signatureAlgorithm) {
-        Key signingKey = generateKeyFromString(privateKey, signatureAlgorithm);
-
+    public static String issueJwt(JsonObject payload, Long expiresAfter, Key privateOrSecretKey, SignatureAlgorithm signatureAlgorithm) {
         long iat = Instant.now().getEpochSecond() * 1000;
         long exp;
 
@@ -89,22 +87,20 @@ public class JwtUtils {
             jwtBuilder.setExpiration(new Date(exp));
         }
 
-        return jwtBuilder.signWith(signingKey).compact();
+        return jwtBuilder.signWith(privateOrSecretKey).compact();
     }
 
     /**
      *
-     * @param publicKey the public key (public/private keypair used to sign the payload)
+     * @param publicOrSecretKey the public/secret key used to sign the payload
      * @param signatureAlgorithm the algorithm used for the  payload signature
      * @param jwtToken the JWS token
      * @return claims
      * @throws JwtException Exception ...
      */
-    public static Claims decodeJwt(String publicKey, SignatureAlgorithm signatureAlgorithm, String jwtToken) throws JwtException {
-        Key key = generateKeyFromString(publicKey, signatureAlgorithm);
-
+    public static Claims decodeJwt(Key publicOrSecretKey, SignatureAlgorithm signatureAlgorithm, String jwtToken) throws JwtException {
         return Jwts.parserBuilder()
-                .setSigningKey(key)
+                .setSigningKey(publicOrSecretKey)
                 .build()
                 .parseClaimsJws(jwtToken)
                 .getBody();
